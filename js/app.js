ac01282b9a2a9f2ca1a01a90bb4350b71894e44f
Nuno Pedro Ribeiro Gago <nprgago@gmail.com>
2018-04-30 19:58:53 +0100

    feat: set and creation of enemy object

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // Setting Enemy Image
     this.sprite = 'images/enemy-bug.png';
+    // Enemy Positions (Random)
+    this.x = Math.floor(Math.random() * (-100 + 1));
+    this.y = (function () {
+      const yPositions = [60, 145, 230];
+      return yPositions[Math.floor(Math.random() * yPositions.length)];
+    })();
+    // Set random enemy speed (between 1 to 5)
+    this.speed = (function () {
+      const speed = Math.floor(Math.random() * 5) + 1;
+      return speed;
+    })();
 };
 
 // Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  if (this.x < 600) {
+    this.x =  (this.x + (1 * dt)) + this.speed;
+    // Enemy-Player Collision Mechanism
+    if (this.y === player.y && this.x > (player.x - 50) && this.x < (player.x + 50)) {
+      player.x = 200;
+      player.y = 400;
+    };
+  // If enemy is offscreen reset position to a random value between -499 and 0
+  } else {
+    this.x = Math.floor(Math.random() * (-500 + 1));
+  }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -26,9 +40,15 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Instantiate of objects.
+let allEnemies = [];
+// Set number of enemies
+const enemiesNumber = 5;
+// Create enemies objects and append them to list
+for (let i = 0; i < enemiesNumber; ++i) {
+  let enemy = new Enemy;
+  allEnemies.push(enemy);
+};
 
 
 
@@ -41,6 +61,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
