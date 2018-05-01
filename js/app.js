b8bbfb546a8ff8c04948b4b10cf4a4f2ee864280
Nuno Pedro Ribeiro Gago <nprgago@gmail.com>
2018-05-01 10:23:01 +0100

    feat: score, win and lose mechanism

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var Enemy = function() {
     })();
     // Set random enemy speed (between 1 to 5)
     this.speed = (function () {
-      const speed = Math.floor(Math.random() * 500) + 1;
+      const speed = Math.floor(Math.random() * 100) + 1;
       return speed;
     })();
 };
@@ -22,6 +22,7 @@ Enemy.prototype.update = function(dt) {
     this.x =  (this.x + 1 + (dt * this.speed));
     // Enemy-Player Collision Mechanism
     if (this.y === player.y && this.x > (player.x - 50) && this.x < (player.x + 50)) {
+      player.level(false);
       player.x = 200;
       player.y = 400;
     };
@@ -61,6 +62,23 @@ var Player = function () {
     if (key === 'right') { this.x += 100; };
     if (key === 'down') { this.y += 85; };
   };
+  this.score = 0;
+  this.level = function (mode = true) {
+    if (mode) {
+      //If true: increment score, speed by 1,5 and display winning alert
+      ++this.score;
+      for (bugEnemy of allEnemies) { bugEnemy.speed = bugEnemy.speed * 1.5; };
+      alert(`Great! You have LEVEL UP!\nYour score is ${this.score}`);
+    } else {
+      //If false and > 0: decrement score, speed by 1,5 and display lossing alert
+      //If false and 0:  only display lossing alert
+      if (this.score > 0) {
+        --this.score;
+        for (bugEnemy of allEnemies) { bugEnemy.speed = bugEnemy.speed / 1.5; };
+        alert(`Ops! Try to avoid all the enemies bugs!\nYour score is ${this.score}.`);
+      } else { alert(`Ops! Try to avoid all the enemies bugs!\nYour score is ${this.score}.`); }
+    }
+  }
 };
 
 // Player prototype inheritance
@@ -71,6 +89,7 @@ Player.prototype.update = function () {
   if (this.y > 400) { this.y = 400; } ;
   // If player arrives to water, he wins and position is reset to initial values.
   if (this.y < 60 ) {
+    this.level();
     this.x = 200;
     this.y= 400;
   } ;
